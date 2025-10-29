65d1857059a629ed75ada6b364132852da16e252
waltercywong <walter21wong@gmail.com>
2025-10-29 10:02:16 -0700

    Add HW5 starter code.

--- HW4/knn_distance.py
+++ HW4/knn_distance.py
@@ -2,5 +2,6 @@ import random
 
 def knn_distance(arr, q, k):
     """Compute the kth nearest point and the distance to it."""
-    
+    # TODO: Implement the knn_distance function
+
     return None
