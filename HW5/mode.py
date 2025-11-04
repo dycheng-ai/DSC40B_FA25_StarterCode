7d11c53142eb1f81799f22865d7baaddbca4f9fe
dycheng-ai <dycheng@ucsd.edu>
2025-11-04 14:51:01 -0800

    finished

--- HW5/mode.py
+++ HW5/mode.py
@@ -2,6 +2,20 @@ def mode(numbers):
     '''
     Efficiently finds the mode of a list of numbers.
     '''
-    #TODO: Implement the mode function
-
-    return None
+    counts = {}
+    for num in numbers:
+        if num in counts:
+            counts[num] += 1
+        else: 
+            counts[num] = 1
+    
+    mode = -999999
+    max_val = -999999
+    values = list(counts.values())
+    keys = list(counts.keys())
+    for i in range(len(values)):
+        if values[i] > max_val:
+            mode = keys[i]
+            max_val = values[i]
+    
+    return mode
