3f81d47a3e4f24ab3736301b7a03638920590e8d
dycheng-ai <dycheng@ucsd.edu>
2025-11-12 22:57:39 -0800

    finished

--- HW6/assign_good_and_evil.py
+++ HW6/assign_good_and_evil.py
@@ -1,7 +1,33 @@
+from collections import deque
+
 def assign_good_and_evil(graph):
     '''
     Assigns good and evil labels to nodes in a graph.
     '''
-    # TODO: Implement the assign_good_and_evil function
+    assignment = {}
+    visited = set()
     
-    return None
+    nodes = list(graph.nodes())
+    
+    for start in nodes:
+        if start in visited:
+            continue
+            
+        queue = deque([start])
+        assignment[start] = 'good'
+        visited.add(start)
+        
+        while queue:
+            current = queue.popleft()
+            current_color = assignment[current]
+            next_color = 'evil' if current_color == 'good' else 'good'
+            
+            for neighbor in graph.neighbors(current):
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    assignment[neighbor] = next_color
+                    queue.append(neighbor)
+                elif assignment[neighbor] == current_color:
+                    return None
+    
+    return assignment
