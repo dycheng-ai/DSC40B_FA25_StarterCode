bd9364fe359f2003db0456adf8b1399b39f242c9
waltercywong <walter21wong@gmail.com>
2025-11-25 13:57:30 -0800

    Add Super Homework starter code.

---
+++ Super Homework/min_ell_theta.py
@@ -0,0 +1,31 @@
+def min_ell_theta(data, colors, cstar):
+    '''
+    Finds theta that minimizes the loss function L(theta) for sorted data.
+    '''
+    # TODO: Implement the min_ell_theta function
+    return None
+
+
+def compute_ell(data, colors, theta):
+    '''
+    Computes the loss function L(theta) for a given theta.
+    '''
+    # TODO: Implement the compute_ell function
+    return None
+
+
+def minimize_ell(data, colors):
+    '''
+    Finds theta that minimizes the loss function L(theta) using quadratic time complexity.
+    '''
+    # TODO: Implement the minimize_ell function
+    return None
+
+
+def minimize_ell_sorted(data, colors):
+    '''
+    Finds theta that minimizes the loss function L(theta) in linear time.
+    '''
+    # TODO: Implement the minimize_ell_sorted function
+    return None
+
